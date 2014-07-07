82b25cf22845ac4313f91c2d8777d42c122bd405
shea@sheasilverman.com <shea@sheasilverman.com>
2014-07-07 14:37:24 +0000

    mame_install

---
+++ mame_install.py
@@ -0,0 +1,70 @@
+# Import the module
+import subprocess
+
+print "Downloading and Installing MAME"
+
+download_and_install = '''
+####MAME START#####
+cd /home/pi/pimame
+#wget http://sheasilverman.com/rpi/raspbian/debs/advancemame_1.2-1_armhf.deb
+wget http://sheasilverman.com/rpi/raspbian/debs/advancemame-raspberrypi_1-1_armhf.deb
+wget http://sheasilverman.com/rpi/raspbian/debs/advancemenu_2.6-1_armhf.deb
+wget http://sheasilverman.com/rpi/raspbian/debs/advancemess_0.102.0.1-1_armhf.deb
+sudo dpkg --force-overwrite -i advancemenu_2.6-1_armhf.deb 
+sudo dpkg --force-overwrite -i advancemame-raspberrypi_1-1_armhf.deb
+sudo dpkg --force-overwrite -i advancemess_0.102.0.1-1_armhf.deb
+rm advancemame-raspberrypi_1-1_armhf.deb 
+rm advancemenu_2.6-1_armhf.deb 
+rm advancemess_0.102.0.1-1_armhf.deb
+#rm advancemame_1.2-1_armhf.deb
+
+###mame4all
+cd /home/pi/pimame
+git clone https://code.google.com/p/mame4all-pi/
+mkdir /home/pi/pimame/emulators/mame4all-pi/
+cp mame4all-pi/mame4all_pi.zip /home/pi/pimame/emulators/mame4all-pi/
+cd /home/pi/pimame/emulators/mame4all-pi/
+unzip -o mame4all_pi.zip
+cd /home/pi/pimame
+rm -rf mame4all-pi/
+
+####MAME END#########
+cd /home/pi/pimame/pimame-menu/
+'''
+
+# Set up the echo command and direct the output to a pipe
+subprocess.call(download_and_install, shell=True)
+
+print "Updating PiPlay Configuration File"
+f = open('/home/pi/pimame/pimame-menu/config.yaml', 'r')
+config_file = f.readlines()
+
+#print config_file
+found_lines = []
+count = 0
+for x in config_file:
+    if "AdvMAME" in x:
+        found_lines.append(count)
+    if "MAME4All" in x:
+        found_lines.append(count)
+    if "Install MAME" in x:
+        found_lines.append(count)
+    count += 1
+
+#print found_lines, count
+
+for x in found_lines:
+    if "visible" in config_file[x + 1]:
+        if "No" in config_file[x + 1]: 
+            print "Making Item Visible"
+            config_file[x + 1] = config_file[x + 1].replace("No","Yes")
+        if "Yes" in config_file[x + 1]: 
+            print "Making Item Invisible"
+            config_file[x + 1] = config_file[x + 1].replace("Yes","No")
+
+f = open('/home/pi/pimame/pimame-menu/config.yaml', 'w')
+for item in config_file:
+    f.write("%s" % item)
+
+print "All Finished!"
+
