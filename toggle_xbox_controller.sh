a2645350a18d34896f21edfac4c0c4077c599af5
shea@sheasilverman.com <shea@sheasilverman.com>
2014-07-11 01:29:02 +0000

    fixes

--- toggle_xbox_controller.sh
+++ toggle_xbox_controller.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if grep --quiet xboxdrv /home/pi/.profile; then
+if grep --quiet 'xboxdrv' /home/pi/.profile; then
   echo "xboxdrv already exists, removing."
-  sed -i '/sudo xboxdrv --silent --config /home/pi/pimame_files/xboxdrv_mapping.cfg --dbus session &/d' /home/pi/.profile
+  #sed -i '/sudo xboxdrv --silent --config /home/pi/pimame_files/xboxdrv_mapping.cfg --dbus session &/d' /home/pi/.profile
+  grep -v "xboxdrv" /home/pi/.profile > profile.temp && rm /home/pi/.profile && mv profile.temp /home/pi/.profile
 else
   echo "Activating Xboxdrv.  Please restart"
-  echo 'sudo xboxdrv --silent --config /home/pi/pimame/config/xboxdrv_mapping.cfg --dbus session &' >> /home/pi/.profile
+  LINE=$(awk '/launchmenu/{print NR}' /home/pi/.profile)
+  echo $LINE
+  LINE=$((LINE-1))
+  echo $LINE
+  #sed '{$LINE}isudo xboxdrv --silent --config /home/pi/pimame/config/xboxdrv_mapping.cfg --dbus session &' /home/pi/.profile
+  awk -v n=$LINE -v s="sudo xboxdrv --silent --config /home/pi/pimame/config/xboxdrv_mapping.cfg --dbus session &" 'NR == n {print s} {print}' /home/pi/.profile > profile.temp && rm /home/pi/.profile && mv profile.temp /home/pi/.profile
+  #echo 'sudo xboxdrv --silent --config /home/pi/pimame/config/xboxdrv_mapping.cfg --dbus session &' >> /home/pi/.profile
 fi
